7eb45e105207f9fc2a2c48b078ef3532913a17fd
minkkjean <mkkjin@g.shingu.ac.kr>
2025-09-17 15:51:38 +0900

    Feat 정렬 알고리즘

---
+++ Game_Algorithm/Assets/Scripts/BubbleSortTest.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSortTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        int[] data = GenerateRandomArrary(100);
+        StartBubbleSort(data);
+            
+        foreach (var item in data)
+        {
+            Debug.Log(item);
+        }
+    }
+
+    int[] GenerateRandomArrary(int size)
+    {
+        int[] arr = new int[size];
+        System.Random rand = new System.Random();
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = rand.Next(0, 10000);
+        }
+        return arr;
+    }
+
+    public static void StartBubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            bool swapped = false;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            //이미 정렬된 경우 조기 종료
+            if (swapped) break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
